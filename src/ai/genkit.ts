81cc43ca79cddf63a6a686d7839c36e3411b4b7f
ezequiel antunes <ezequielantunes07@gmail.com>
2025-12-30 19:13:31 +0000

    erro

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,8 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({
-      apiKey: process.env.GEMINI_API_KEY
-  })],
-  // The model is specified in the prompt/generation call
+  plugins: [googleAI()],
+  // O modelo é especificado na chamada de prompt/geração
 });
